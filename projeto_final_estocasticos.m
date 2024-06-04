27c96beb15f4ad70a2a83d92aa90a760f952a04d
thais13bm <110418856+thais13bm@users.noreply.github.com>
2024-06-04 18:40:35 -0300

    plots for question 3

--- projeto_final_estocasticos.m
+++ projeto_final_estocasticos.m
@@ -4,8 +4,7 @@
 
 ####blz mas n eh pra entender oq falaram, eh so pra separar as vozes
 
-
-
+pkg load signal;
 load Mic1.mat;
 load Mic2.mat;
 load Mic3.mat;
@@ -20,6 +19,25 @@ sound(Mic2,Fs)
 sound(Mic3,Fs)
 
 sound(Parcial,Fs)
+
+
+
+###questao 3
+[acf1,lags1] = xcorr(Mic1,'normalized');
+[acf2,lags2] = xcorr(Mic2,'normalized');
+[acf3,lags3] = xcorr(Mic3,'normalized');
+[acf4,lags4] = xcorr(Parcial,'normalized');
+
+
+figure(1)
+plot(lags1,acf1)
+figure(2)
+plot(lags2,acf2)
+figure(3)
+plot(lags3,acf3)
+figure(4)
+plot(lags4,acf4)
+
 
 
 
