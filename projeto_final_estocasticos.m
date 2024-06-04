057da383c8aa2ade7d22bbf8894e94fefb700d5b
thais13bm <110418856+thais13bm@users.noreply.github.com>
2024-06-04 16:47:54 -0300

    add simple code reading the .mat files

---
+++ projeto_final_estocasticos.m
@@ -0,0 +1,25 @@
+###acho que sao 3 pessoas(2 homens e 1 mulher)
+
+###look in the corner to find the..blabla shirt...nine men unica coisa q entendi
+
+####blz mas n eh pra entender oq falaram, eh so pra separar as vozes
+
+#### o mic3 eh igual a voz_conhecida?
+
+load Mic1.mat;
+load Mic2.mat;
+load Mic3.mat;
+load Voz_conhecida.mat;
+
+
+
+sound(Mic1,Fs)
+
+sound(Mic2,Fs)
+
+sound(Mic3,Fs)
+
+sound(Mic3,Fs)
+
+
+
