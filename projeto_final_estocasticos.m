b4748583ed39d720e6f52a7b268586cac346f189
thais13bm <110418856+thais13bm@users.noreply.github.com>
2024-06-05 21:32:42 -0300

    question 4: power spectral density

--- projeto_final_estocasticos.m
+++ projeto_final_estocasticos.m
@@ -29,6 +29,8 @@ sound(Parcial,Fs)
 [acf4,lags4] = xcorr(Parcial,'normalized');
 
 
+
+
 figure(1)
 plot(lags1,acf1)
 figure(2)
@@ -38,6 +40,49 @@ plot(lags3,acf3)
 figure(4)
 plot(lags4,acf4)
 
+
+####pergunta da questao: posso concluir que eh aproximadamente estacionario?tem as propriedades quando é estacionario: a funçao é par, o valor maximo é no zero...
+####e tipo, so faz sentido pedir a PSD se eh estacionario
+
+####questao 4
+
+[pxx1, f1] = pwelch(Mic1, Fs=Fs);  % Calcula a PSD para Mic1
+[pxx2, f2] = pwelch(Mic2, Fs=Fs);  % Calcula a PSD para Mic2
+[pxx3, f3] = pwelch(Mic3, Fs=Fs);  % Calcula a PSD para Mic3
+[pxx4, f4] = pwelch(Parcial, Fs=Fs);  % Calcula a PSD para Parcial
+
+
+% Plotar a PSD para Mic1
+figure(5)
+plot(f1, 10*log10(pxx1));    ##lembrando que a PSD eh no dominio da frequencia
+xlabel('Frequência (Hz)');
+ylabel('Densidade Espectral de Potência (dB/Hz)');
+title('Densidade Espectral de Potência (PSD) - Mic1');
+grid on;
+
+% Plotar a PSD para Mic2
+figure(6)
+plot(f2, 10*log10(pxx2));
+xlabel('Frequência (Hz)');
+ylabel('Densidade Espectral de Potência (dB/Hz)');
+title('Densidade Espectral de Potência (PSD) - Mic2');
+grid on;
+
+% Plotar a PSD para Mic3
+figure(7)
+plot(f3, 10*log10(pxx3));
+xlabel('Frequência (Hz)');
+ylabel('Densidade Espectral de Potência (dB/Hz)');
+title('Densidade Espectral de Potência (PSD) - Mic3');
+grid on;
+
+% Plotar a PSD para Parcial
+figure(8)
+plot(f4, 10*log10(pxx4));
+xlabel('Frequência (Hz)');
+ylabel('Densidade Espectral de Potência (dB/Hz)');
+title('Densidade Espectral de Potência (PSD) - Parcial');
+grid on;
 
 
 
